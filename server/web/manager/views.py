cb3eb39f10ce059f8cadf43488a512c4646f8860
Yuka Matsuo <matsuo.yk@gmail.com>
2017-10-29 21:50:28 +0900

    HTML/CSSから必要ない記述を削除

--- server/web/manager/views.py
+++ server/web/manager/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from .models import Sensor
 
 
-# DBから過去10件の温度と湿度を取得して、その値からsuperdryの時間をテンプレートに渡す
 def print_time(request):
+    """DBから過去10件の温度と湿度を取得して、その値からsuperdryの時間をテンプレートに渡す"""
     sensor = Sensor.objects.order_by('-created').first()
     if sensor is None:
         return render(request, 'default.html')
